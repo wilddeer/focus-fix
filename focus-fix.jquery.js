307bb306cf189073612b7177398409d8bcffee7a
Oleg Korsunsky <jatruadmin@gmail.com>
2014-12-19 00:40:35 +0300

    add jQuery focus fix

---
+++ focus-fix.jquery.js
@@ -0,0 +1,20 @@
+/* focus-fix | https://github.com/wilddeer/focus-fix/ | CC0 */
+(function() {
+    var mouseFocusedClass = 'is-mouse-focused';
+
+    $(document.body).on('mousedown', function(event) {
+        setTimeout(function() {
+            //find focused element
+            var activeElement = document.activeElement,
+                $activeElement = $(activeElement);
+
+            //if found and it's not body...
+            if (activeElement && activeElement !== document.body) {
+                //add special class, remove it after `blur`
+                $activeElement.addClass(mouseFocusedClass).one('blur', function() {
+                    $activeElement.removeClass(mouseFocusedClass);
+                });
+            }
+        }, 0);
+    });
+})();
