ee41ae650b900e12f9828babfad04b7c5daf0398
Oleg Korsunsky <jatruadmin@gmail.com>
2014-12-19 00:41:30 +0300

    add vanilla focus fix

---
+++ focus-fix.js
@@ -0,0 +1,22 @@
+/* focus-fix | https://github.com/wilddeer/focus-fix/ | CC0 */
+(function() {
+    var mouseFocusedClass = 'is-mouse-focused';
+
+    document.body.addEventListener('mousedown', function(event) {
+        setTimeout(function() {
+            //find focused element
+            var activeElement = document.activeElement;
+
+            //if found and it's not body...
+            if (activeElement && activeElement !== document.body) {
+                //add special class, remove it after `blur`
+                activeElement.className += ' ' + mouseFocusedClass;
+                activeElement.addEventListener('blur', function(e) {
+                    e.target.removeEventListener(e.type, arguments.callee);
+
+                    activeElement.className = activeElement.className.replace(new RegExp('(\\s+|^)'+mouseFocusedClass+'(\\s+|$)', 'g'), ' ').replace(/^\s+|\s+$/g, '');
+                });
+            }
+        }, 0);
+    });
+})();
