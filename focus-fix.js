763b793310be65ebff6c4c5cbe48a90ebf18a054
Oleg Korsunsky <jatruadmin@gmail.com>
2014-12-19 00:51:26 +0300

    additional comment and cleanup

--- focus-fix.js
+++ focus-fix.js
@@ -1,8 +1,9 @@
 /* focus-fix | https://github.com/wilddeer/focus-fix/ | CC0 */
 (function() {
     var mouseFocusedClass = 'is-mouse-focused';
-
+    
     document.body.addEventListener('mousedown', function(event) {
+        //wait for `document.activeElement` to change
         setTimeout(function() {
             //find focused element
             var activeElement = document.activeElement;
@@ -11,6 +12,7 @@
             if (activeElement && activeElement !== document.body) {
                 //add special class, remove it after `blur`
                 activeElement.className += ' ' + mouseFocusedClass;
+                
                 activeElement.addEventListener('blur', function(e) {
                     e.target.removeEventListener(e.type, arguments.callee);
 
