d3e7168c3e2d6d1fbd0ac3d4d1f7b185c2208167
Oleg Korsunsky <jatruadmin@gmail.com>
2014-12-26 21:38:02 +0300

    remove unnecessary `event` argument

--- focus-fix.js
+++ focus-fix.js
@@ -2,7 +2,7 @@
 (function() {
     var mouseFocusedClass = 'is-mouse-focused';
     
-    document.body.addEventListener('mousedown', function(event) {
+    document.body.addEventListener('mousedown', function() {
         //wait for `document.activeElement` to change
         setTimeout(function() {
             //find focused element
